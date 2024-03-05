7e61d4f7fc3d1e2b64db8bf9808424d91f5ad8d8
dacevedo-UMD <dacevedo@terpmail.umd.edu>
2024-03-05 15:41:33 -0500

    Finish the project

--- Projects/Project 3/PhotoViewer/code.js
+++ Projects/Project 3/PhotoViewer/code.js
@@ -2,6 +2,7 @@
 
 let photoArr = new Array();
 let currIdx = 0;
+let x = 0;
 let animationId, slideShowStarted, randomSlideShowStarted = false;
 const speed = 1000;
 
@@ -9,6 +10,7 @@ document.querySelector("#LoPbutton").addEventListener("click", loadPhoto);
 document.querySelector("#LJbutton").addEventListener("click", loadJSON);
 document.querySelector("#PPbutton").addEventListener("click", previousPhoto);
 document.querySelector("#NPbutton").addEventListener("click", nextPhoto);
+document.querySelector("#photo").addEventListener("click", nextPhoto);
 document.querySelector("#FPbutton").addEventListener("click", firstPhoto);
 document.querySelector("#LaPbutton").addEventListener("click", lastPhoto);
 document.querySelector("#SSbutton").addEventListener("click", slideShow);
@@ -22,16 +24,13 @@ function loadPhoto() {
     let end = Number(document.querySelector("#endNum").value);
 
     // checks to make sure that both start and end are in range
-    if (start < 1 || end > 6 || end - start < 0) {
+    if (end < start) {
         let statusElement = document.querySelector("#photo-status");
         statusElement.textContent = "Error: Invalid Range";
         return;
-    } else {
-        let statusElement = document.querySelector("#photo-status");
-        statusElement.textContent = "Photo Viewer System";
-    }
+    } 
 
-    photoArr = new Array();                                         // to reset maybe??
+    photoArr = new Array();                                         // to reset maybe?? does JS auto collect garbage like java??
     for(let idx = start; idx < end + 1; idx++) {
         photoArr.push(folder + name + idx + ".jpg");
     }
@@ -41,6 +40,12 @@ function loadPhoto() {
 }
 
 function previousPhoto() {
+    if (photoArr.length == 0) {
+        let statusElement = document.querySelector("#photo-status");
+        statusElement.textContent = "Error: you must load data first";
+        return;
+    }
+
     if (currIdx - 1 < 0) {
         currIdx = photoArr.length - 1;
     } else {
@@ -50,6 +55,12 @@ function previousPhoto() {
 }
 
 function nextPhoto() {
+    if (photoArr.length == 0) {
+        let statusElement = document.querySelector("#photo-status");
+        statusElement.textContent = "Error: you must load data first";
+        return;
+    }
+
     if (currIdx + 1 == photoArr.length) {
         currIdx = 0;
     } else {
@@ -59,20 +70,33 @@ function nextPhoto() {
 }
 
 function firstPhoto() {
-    currIdx = 0;
+    if (photoArr.length == 0) {
+        let statusElement = document.querySelector("#photo-status");
+        statusElement.textContent = "Error: you must load data first";
+        return;
+    }
+
+    setIdxToZero();
     displayPhoto();
 }
 
+/* LAMBDA */ /* LAMBDA */ /* LAMBDA */ 
+let setIdxToZero = () => currIdx = 0;
+
 function lastPhoto() {
+    if (photoArr.length == 0) {
+        let statusElement = document.querySelector("#photo-status");
+        statusElement.textContent = "Error: you must load data first";
+        return;
+    }
+
     currIdx = photoArr.length - 1;
     displayPhoto();
 }
 
 function displayPhoto() {
-    if (photoArr.length == 0) {
-        let statusElement = document.querySelector("#photo-status");
-        statusElement.textContent = "Error: you must load data first";
-    }
+    let statusElement = document.querySelector("#photo-status");
+    statusElement.textContent = "Photo Viewer System";
 
     let imageElement = document.getElementById("photo");
     imageElement.setAttribute("src", photoArr[currIdx]);
@@ -96,20 +120,45 @@ function loadJSON() {
 }
 
 function slideShow() {
-    if (!slideShowStarted && !randomSlideShowStarted) { // preventing execuiting setInterval multiple times before stopping animation
+    if (photoArr.length == 0) {
+        let statusElement = document.querySelector("#photo-status");
+        statusElement.textContent = "Error: you must load data first";
+        return;
+    }
+
+    if (!slideShowStarted && !randomSlideShowStarted) { 
         animationId = setInterval("nextPhoto()", speed);
         slideShowStarted = true;
     }
 }
 
 function randomSlideShow() {
-    if (!slideShowStarted && !randomSlideShowStarted) { // preventing execuiting setInterval multiple times before stopping animation
-        animationId = setInterval("displayPhoto()", speed);
+    if (photoArr.length == 0) {
+        let statusElement = document.querySelector("#photo-status");
+        statusElement.textContent = "Error: you must load data first";
+        return;
+    }
+
+    if (!slideShowStarted && !randomSlideShowStarted) { 
+        animationId = setInterval("randomPhoto()", speed);
         randomSlideShowStarted = true;
     }
 }
 
+
+
+function randomPhoto() {
+    currIdx = Math.floor(Math.random() * photoArr.length); 
+    displayPhoto();
+}
+
 function stopSlideShow() {
+    if (photoArr.length == 0) {
+        let statusElement = document.querySelector("#photo-status");
+        statusElement.textContent = "Error: you must load data first";
+        return;
+    }
+
     if (slideShowStarted || randomSlideShowStarted) {
         clearInterval(animationId);
         slideShowStarted = false;
